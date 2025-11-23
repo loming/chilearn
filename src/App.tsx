9ed6f1bdf3fb798e3edf9a49102587667081305e
Lo Ming <loming@loming.com>
2025-11-23 02:47:18 +0000

    fix front page animated cloud

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,32 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { GameProvider } from './context/GameContext';
-import { Layout } from './components/Layout';
-import { ChapterSelect } from './pages/ChapterSelect';
-import { LearningSession } from './pages/LearningSession';
-
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+import { ChapterSelect } from './pages/ChapterSelect'
+import { LearningSession } from './pages/LearningSession'
+import { GameProvider } from './context/GameContext'
+import './App.css'
 import { CloudBackground } from './components/CloudBackground'
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<ChapterSelect />} />
+        <Route path="/learn/:chapterId" element={<LearningSession />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 function App() {
   return (
-<<<<<<< HEAD
     <GameProvider>
       <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<ChapterSelect />} />
-            <Route path="/learn/:chapterId" element={<LearningSession />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </Layout>
+        <CloudBackground />
+        <AnimatedRoutes />
       </Router>
     </GameProvider>
-  );
-=======
-    <SoundProvider>
-      <GameProvider>
-        <Router>
-          <CloudBackground />
-          <MusicToggle />
-          <AnimatedRoutes />
-        </Router>
-      </GameProvider>
-    </SoundProvider>
   )
->>>>>>> b0c222b (Animated cloud background and new image assets)
 }
 
-export default App;
+export default App
